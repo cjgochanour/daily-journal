ff5bb24a9f1537714c0e09e70a22319b67c2f5c5
cjgochanour <cjgochanour@gmail.com>
2022-01-02 23:26:46 -0600

    refactored to use persistant API rather than flat-data

--- scripts/database.js
+++ scripts/database.js
@@ -1,34 +1,13 @@
-const database = {
-    entries: [
-        {
-            id: 1,
-            date: "07/24/2025",
-            concept: "HTML & CSS",
-            entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-            mood: "Glad",
-        },
-        {
-            id: 2,
-            date: "07/26/2025",
-            concept: "JavaScript",
-            entry: "We learned how to manage a flat-structure database.",
-            mood: "Glad",
-        },
-        {
-            id: 3,
-            date: "07/28/2025",
-            concept: "APIs",
-            entry: "We talked about using APIs as persistent data.",
-            mood: "Sad",
-        },
-        {
-            id: 4,
-            date: "07/31/2025",
-            concept: "React",
-            entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-            mood: "Glad",
-        },
-    ],
+const API = "http://localhost:8088";
+
+let localEntries = [];
+
+export const getEntries = () => {
+    return fetch(`${API}/entries`)
+        .then((res) => res.json())
+        .then((entries) => {
+            localEntries = entries;
+        });
 };
 
-export const getJournalEntries = () => database.entries.map((entry) => ({ ...entry }));
+export const getJournalEntries = () => localEntries.map((entry) => ({ ...entry }));
